8cb32a95be478ecc04b3d07cecc71520c3a02022
Paweł Biernat <pawel.biernat@gmail.com>
2011-05-04 09:27:26 +0200

    Almost

--- src/mesh/mesh.f90
+++ src/mesh/mesh.f90
@@ -1,5 +1,19 @@
-module mesh
+module mesh_module
   type mesh
-     real 
+     integer :: dim, nx, nf, maxrk
+     real, allocatable :: x(:)
+     real, allocatable :: f(:,:)
+     real, allocatable :: df(:,:,:)
   end type mesh
-end module mesh
+contains
+
+  function MeshInit(nx, nf, maxrk)
+    integer :: nx,nf,maxrk
+    type(mesh) :: MeshInit(1, nx, nf, maxrk)
+
+    allocate(MeshInit%x(nx))
+    allocate(MeshInit%f(nf,nx))
+    allocate(MeshInit%df(maxrk,nf,nx))
+  end function MeshInit
+
+end module mesh_module
