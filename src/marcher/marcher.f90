9d45a19767024c7ec6986c6416d5f4aed6de4d0f
Paweł Biernat <pawel.biernat@gmail.com>
2011-05-05 16:40:38 +0200

    added marcher file

---
+++ src/marcher/marcher.f90
@@ -0,0 +1,10 @@
+module module_marcher
+  type marcher
+     real, allocatable :: f
+     real, allocatable :: df
+     real, allocatable :: t
+     real, allocatable :: dt
+   contains
+
+  end type marcher
+end module module_marcher
