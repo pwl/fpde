9c242cfa5f2b817eca6ec5d6873365b73a58734f
Paweł Biernat <pawel.biernat@gmail.com>
2011-10-02 21:46:49 +0200

    generate_png.py can now take several use instructions

--- scripts/generate_png.py
+++ scripts/generate_png.py
@@ -1,28 +1,49 @@
 from argparse import ArgumentParser
 import os
-import sys
 import re
+import shutil
+import sys
 
-
-def main(dir, out, options):
+# @todo plot several functions
+def main(files, out, options):
     out.write("set terminal png\n")
     using = options.using
     wit   = options.wit
     title = options.title
-    for filename in (f for f in os.listdir(dir) if f.endswith(".dat")):
-        filename = os.path.join(dir, filename)
+    n = len(files)
+    print "Files to process: {0}".format(n)
+
+    for i, filename in enumerate(files):
         text = open(filename).read()
         data_vars = get_vars(text)
 
+        out.write('print "{0}/{1}"\n'.format(i,n))
         out.write('set output "{0}.png"\n'.format(filename))
-        out.write('plot "{0}" u {using} w {wit} t {title}\n'.\
-                      format(filename,\
-                                 using=using.format(**data_vars),\
-                                 wit=wit,\
-                                 title=title.format(**data_vars)))
+        out.write('plot "{0}" u {using} w {wit} t "{title}"'.\
+                      format(filename,
+                             using=using[0].format(**data_vars),
+                             wit=wit,
+                             title=title[0].format(**data_vars)))
+
+        for u, t in zip(using[1:],title[1:]):
+            u = u.format(**data_vars)
+            t = t.format(**data_vars)
+            out.write(', "" u {using} w {wit} t "{title}"'.\
+                          format(using=u,
+                                 wit=wit,
+                                 title=t))
+        out.write('\n')
 
     out.close()
     os.system("gnuplot {0}".format(out_filename))
+
+    # @todo set apropriate system path in CMakeList.txt
+    if options.movie:
+        os.system("../fpde/scripts/make_movie.sh {0}".format(dir))
+
+def list_dat(dir, format=".dat"):
+    return [os.path.join(dir, f) \
+                for f in os.listdir(dir) if f.endswith(format)]
 
 def get_vars(text):
     vars = {}
@@ -40,12 +61,22 @@ if __name__ == "__main__":
     parser.add_argument("-w", "--with", dest="wit",
                         help="Set with", default="lp")
     parser.add_argument("-t", "--title", dest="title",
-                        help="Set title", default='"title"')
+                        help="Set title", default='')
+    parser.add_argument("-m", "--movie", dest="movie",
+                        help="Generate movie?", action="store_true")
+
 
     options = parser.parse_args()
 
+    options.using = options.using.split("|")
+    titles = options.title.split("|")
+    options.title = [""] * len(options.using)
+    options.title[:len(titles)] = titles
+
     dir = options.dir
+    [os.remove(f) for f in list_dat(dir,".png")]
+
     out_filename = "plot.gp"
     out = open(out_filename, 'w')
 
-    main(dir, out, options)
+    main(list_dat(dir), out, options)
--- scripts/make_movie.sh
+++ scripts/make_movie.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 ls -1 $1/*.png|sort > filelist.txt
-mencoder -ovc lavc -lavcopts vbitrate=800 -mf type=png:fps=30 -o $1/movie.avi mf://@filelist.txt
+mencoder -ovc lavc -lavcopts vbitrate=800 -mf type=png:fps=10 -o $1/movie.avi mf://@filelist.txt
 
 mplayer $1/movie.avi
